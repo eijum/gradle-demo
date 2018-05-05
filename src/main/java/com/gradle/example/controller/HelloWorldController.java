8d8117b880227cbd8f7b504b370ee6eedac46b6c
andrew <andrew@1858.com>
2018-05-05 17:01:10 +0800

    add gradle configuration

--- src/main/java/com/gradle/example/controller/HelloWorldController.java
+++ src/main/java/com/gradle/example/controller/HelloWorldController.java
@@ -5,10 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- * Created by dxjm on 2018/5/5.
- */
-
 
 @Controller
 public class HelloWorldController {
